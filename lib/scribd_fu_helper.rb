b5c4837d9b34541b50f9753c04035253339746cc
jerome <jerome@yayel.com>
2008-09-28 08:29:49 -0700

    Oops

--- lib/scribd_fu_helper.rb
+++ lib/scribd_fu_helper.rb
@@ -2,7 +2,7 @@ module Scribd_fu_Helper
   
   def display_scribd(object, alternate_text = '')
     out = '<script type="text/javascript" src="http://www.scribd.com/javascripts/view.js"></script>'
-    out = out + "<div id="embedded_flash">#{alternate_text}</div>"
+    out = out + %(<div id="embedded_flash">#{alternate_text}</div>)
     out = out + '<script type="text/javascript">'
     out = out + "var scribd_doc = scribd.Document.getDoc(#{object.scribd_id}, '#{object.scribd_access_key}');"
     out = out +	"scribd_doc.write('embedded_flash');"
